232044412579658784c50f85129c7457bfe660f9
Alexander <alexanders.tischner@outlook.com>
2022-10-28 11:58:21 -0600

    setting up routes for notes db

--- server.js
+++ server.js
@@ -7,9 +7,24 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
-// parsing data into json
+// middleware parsing json data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use('/api', api);
+
+
+app.use(express.static('public'));
+
+
+// Get route for main index.html:
+app.get('/', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// Get route for notes.html:
+app.get('/notes', (req, res) => 
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
 
 
